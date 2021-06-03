bd2f09a04a745fbd3b98074dcafded2c71662547
anitabenites <anitabenites2014@gmail.com>
2021-06-03 18:16:56 +0200

    my first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SMSForm from "./SMSForm";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      greeting: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      name: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+      .then((response) => response.json())
+      .then((state) => this.setState(state));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <SMSForm />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
